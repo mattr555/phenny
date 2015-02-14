c4e2f26ba90c0ce4429df9e622731eeee7a315d0
Matthew Ramina <mattramina@gmail.com>
2015-02-14 15:43:20 -0500

    twitch plays ur mom

---
+++ modules/tpp.py
@@ -0,0 +1,67 @@
+import web
+import json
+import time
+import datetime
+import threading
+import os
+
+def filename(phenny):
+    name = phenny.nick + '-' + phenny.config.host + '.tpp.db'
+    return os.path.join(os.path.expanduser('~/.phenny'), name)
+
+def dump(phenny):
+    with open(filename(phenny), 'w') as f:
+        json.dump(phenny.tpplast, f)
+
+def load(phenny):
+    if os.path.exists(filename(phenny)):
+        with open(filename(phenny)) as f:
+            try:
+                phenny.tpplast = json.loads(f.read())
+            except ValueError:
+                phenny.tpplast = []
+    else:
+        phenny.tpplast = []
+    dump(phenny)
+
+def setup(phenny):
+    phenny.tpplasttime = time.time() - 61
+    load(phenny)
+    phenny.tpp_timer = threading.Timer(60, check_new, (phenny,))
+    phenny.tpp_timer.start()
+
+def list_difference(new, old):
+    if len(old) == 0:
+        return new
+    newest = old[0]
+    for i in range(len(new)):
+        if new[i] == newest:
+            return new[:i] + old
+
+def check_new(phenny):
+    r = web.get(phenny.config.tpp_update_url)
+    r = json.loads(r)
+    phenny.tpplast = list_difference(r['data']['children'], phenny.tpplast)
+    phenny.tpplasttime = time.time()
+    phenny.tpp_timer = threading.Timer(60, check_new, (phenny,))
+    phenny.tpp_timer.start()
+
+def get_msg(phenny, input):
+    if not phenny.tpplast:
+        check_new(phenny)
+    if input.group(1) and input.group(1).isdigit():
+        num = int(input.group(1)) - 1
+    else:
+        num = 0
+    if num > len(phenny.tpplast) - 1:
+        return phenny.say('Sorry, too far back.')
+    phenny.say('{} - /u/{}'.format(phenny.tpplast[num]['data']['body'], phenny.tpplast[num]['data']['author']))
+get_msg.rule = r'!update(?:\s(\d+))?'
+
+def get_time(phenny, input):
+    delta = datetime.datetime.now() - datetime.datetime(2015, 2, 12, 16)
+    d = delta.days
+    h = delta.seconds // (60*60)
+    m = delta.seconds // 60 % 60
+    phenny.say('{}d{}h{}m'.format(d, h, m))
+get_time.rule = r'!time'
