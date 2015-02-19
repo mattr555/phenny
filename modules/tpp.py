353afe74545f21be4ffff709d079b4422a9c4c08
Matthew Ramina <mattramina@gmail.com>
2015-02-19 17:24:59 -0500

    don't kill the thread if we fail

--- modules/tpp.py
+++ modules/tpp.py
@@ -20,10 +20,10 @@ def check_new(phenny):
         r = web.get(phenny.config.tpp_update_url)
         r = json.loads(r)
         phenny.tpplast = r['data']['children']
-        phenny.tpp_timer = threading.Timer(60, check_new, (phenny,))
-        phenny.tpp_timer.start()
     except:
         pass
+    phenny.tpp_timer = threading.Timer(60, check_new, (phenny,))
+    phenny.tpp_timer.start()
 
 def get_msg(phenny, input):
     if phenny.tpplasttime + 10 > time.time():
